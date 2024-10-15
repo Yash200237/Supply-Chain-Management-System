a0b8c1e7ead10df5e0f6bae749f8b48ea3f411fe
Samadhi Talagala <154465630+samadhianjana@users.noreply.github.com>
2024-10-15 15:33:16 +0530

    manager route works, driver route doesn't

--- server/routes/ManagerRoute.js
+++ server/routes/ManagerRoute.js
@@ -10,14 +10,20 @@ router.post("/managerlogin", (req, res) => {
   con.query(sql, [req.body.email, req.body.password], (err, result) => {
     if (err) return res.json({ loginStatus: false, Error: "Query error" });
     if (result.length > 0) {
+      const manager = result[0]
       const email = result[0].email;
       const token = jwt.sign(
-        { role: "manager", email: email },
+        { role: "manager", 
+          email: email,
+          manager_ID: manager.manager_ID,
+         },
         "jwt_secret_key",
         { expiresIn: "1d" }
       );
       res.cookie("token", token);
-      return res.json({ loginStatus: true });
+      return res.json({ loginStatus: true,
+        manager_ID: manager.manager_ID,
+        managerName: manager.first_name,});
     } else {
       return res.json({ loginStatus: false, Error: "wrong email or password" });
     }
