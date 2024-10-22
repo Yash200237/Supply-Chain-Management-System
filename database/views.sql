6b2a207cad70fb44bb94f76c72e0bb909e9940b4
Samadhi Talagala <154465630+samadhianjana@users.noreply.github.com>
2024-10-22 15:54:53 +0530

    created view file MySQL views

---
+++ database/views.sql
@@ -0,0 +1,15 @@
+--TrackDelivery View
+CREATE OR REPLACE VIEW TrackDelivery AS
+SELECT 
+    o.date AS `Purchased Date`, 
+    r.path_description AS `Route`, 
+    GROUP_CONCAT(p.name ORDER BY p.name ASC SEPARATOR ', ') AS `Products`,
+    s.status AS `Status`,
+    o.customer_ID,
+    FORMAT(SUM((p.price - (p.price * p.discount / 100)) * op.quantity), 2) AS `Total Price`  -- Format to 2 decimal places
+FROM `order` o
+JOIN route r ON o.route_ID = r.route_ID
+JOIN orderstatus s ON s.status_ID = o.status_ID
+JOIN orderproduct op ON o.order_ID = op.order_id
+JOIN product p ON p.product_ID = op.product_id
+GROUP BY o.order_ID;
