f46336014d7832669a859bf3c9ec7efd30b4025e
asithDilusha <ekanayakalad.22@uom.lk>
2024-10-29 10:22:35 +0530

    Driver manager report setup

--- server/utils/db.js
+++ server/utils/db.js
@@ -3,8 +3,8 @@ import mysql from "mysql2";
 const con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "yashodha",
-  database: "supply_chain_management_system",
+  password: "Asith2002",
+  database: "scms",
   multipleStatements: true, // Enables multiple result sets
 });
 
