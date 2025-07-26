ffe7b590b981b959aee995792a906f88a4dbd7cb
Yash200237 <yashodhathathsarani2002@gmail.com>
2025-07-26 21:09:08 +0530

    UIs added

--- server/utils/db.js
+++ server/utils/db.js
@@ -3,8 +3,8 @@ import mysql from "mysql2";
 const con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "<password>",
-  database: "<database_name>",
+  password: "yashodha",
+  database: "supply_chain_management_system",
   multipleStatements: true, // Enables multiple result sets
 });
 
