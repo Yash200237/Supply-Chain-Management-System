4b2ad3aa8ae4f02418fd71073365870747b8d29d
Yash200237 <yashodhathathsarani2002@gmail.com>
2025-07-26 19:38:58 +0530

    server modified

---
+++ server/utils/db.js
@@ -0,0 +1,19 @@
+import mysql from "mysql2";
+
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "<password>",
+  database: "<database_name>",
+  multipleStatements: true, // Enables multiple result sets
+});
+
+con.connect(function (err) {
+  if (err) {
+    console.log("connection error");
+  } else {
+    console.log("connected");
+  }
+});
+
+export default con;
